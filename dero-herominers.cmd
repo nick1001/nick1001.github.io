3f5b3310dbd185a2acddf6549baaa25e96261bfa
nick1001 <nicholasflyer@yahoo.com>
2024-05-25 04:53:53 -0500

    Delete dero-herominers.cmd

--- dero-herominers.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-./xmrig --donate-level 1 -o us.dero.herominers.com:1117 -p USED100 -u dERoWtoXKM1XkSk8kencLCjHhqqz2Ra5oRa2xMMD6KdWHBP6y22gqdBSY5DCAtWW154rqN8F1pfp5WzZ4SbkPj3b1XFahTQFo3 -a astrobwt -k 
-pause
-
